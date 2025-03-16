a00603c7d62c58f9735a8ebad89436915e282300
kamil <kamilmaslanka212@gmail.com>
2025-03-16 17:01:19 +0100

    clean UI and routing structure before implementing chatGPT api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
 import Logo from "@/components/logo";
-import OpenAI from "openai";
+
 import InputArea from "@/components/input-area";
-
-const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-});
-
-const completion = openai.chat.completions.create({
-  model: "gpt-4o-mini",
-  store: true,
-  messages: [{ role: "user", content: "write a haiku about ai" }],
-});
-
-completion.then((result) => console.log(result.choices[0].message));
 
 export default function Home() {
   return (
--- src/components/input-area.tsx
+++ src/components/input-area.tsx
@@ -2,36 +2,21 @@
 
 import { ChangeEvent, useRef, useState } from "react";
 import { FaArrowUp } from "react-icons/fa";
+import { useChat } from "@ai-sdk/react";
 
 export default function InputArea() {
-  const [text, setText] = useState<string>("");
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-
-  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setText(e.target.value);
-    adjustHeight();
-  };
-
-  const adjustHeight = () => {
-    if (textareaRef.current) {
-      textareaRef.current.style.height = "auto";
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
-    }
-  };
-
   return (
-    <div className="w-full flex items-center rounded-md bg-[#f0f8fa] shadow-lg ">
-      <textarea
-        ref={textareaRef}
-        value={text}
-        onChange={handleInputChange}
-        placeholder="Zapytaj mnie o coś..."
-        className="w-full p-4 bg-transparent outline-none placeholder-gray-400 text-black resize-none overflow-hidden"
-        rows={1}
-      />
-      <button className="w-8 h-8 mr-2 rounded-full flex justify-center items-center hover:bg-gray-200 transition active:bg-gray-300">
-        <FaArrowUp className="text-black" />
-      </button>
-    </div>
+    <>
+      <div className="w-full flex items-center rounded-md bg-[#f0f8fa] shadow-lg ">
+        <textarea
+          placeholder="Zapytaj mnie o coś..."
+          className="w-full p-4 field-sizing-content bg-transparent outline-none placeholder-gray-400 text-black resize-none overflow-hidden"
+          rows={1}
+        />
+        <button className="w-8 h-8 mr-2 rounded-full flex justify-center items-center hover:bg-gray-200 transition active:bg-gray-300">
+          <FaArrowUp className="text-black" />
+        </button>
+      </div>
+    </>
   );
 }
