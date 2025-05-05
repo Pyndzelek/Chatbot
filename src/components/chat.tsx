9e7a2ac7ebac959ab1ff7dac93390db75a8aa42b
kamil <kamilmaslanka212@gmail.com>
2025-05-05 17:35:32 +0200

    Fast API setup

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useChat } from "@ai-sdk/react";
 import { useState } from "react";
 import InputArea from "./input-area";
 import ChatAiAnswer from "./chat-ai-answer";
@@ -8,27 +7,41 @@ import ChatUserPrompt from "./chat-user-prompt";
 
 export default function Chat() {
   const [text, setText] = useState("");
-  const { messages, append, error } = useChat();
+  const [messages, setMessages] = useState([
+    { role: "assistant", message: "Elo elo" },
+  ]);
 
   const handleOnClick = async () => {
-    console.log("handleOnClick");
-    console.log("text", text);
     if (text.trim() === "") return;
-    append({ content: text, role: "user" });
-    console.log("handleOnClick", text);
+    setMessages((prev) => [...prev, { role: "user", message: text }]);
     setText("");
+
+    try {
+      const res = await fetch("");
+      if (!res.ok) throw new Error("Fetch failed");
+      const data = await res.json();
+
+      const newAnswer = data.text || "No answer found";
+
+      setMessages((prev) => [
+        ...prev,
+        { role: "assistant", message: newAnswer },
+      ]);
+    } catch (error) {
+      console.error(error);
+      setMessages((prev) => [...prev, { role: "assistant", message: "ERROR" }]);
+    }
   };
 
   return (
     <>
       {messages.map((m) => (
-        <div key={m.id} className="w-full">
-          {m.role === "user" && <ChatUserPrompt text={m.content} />}
+        <div key={m.message} className="w-full">
+          {m.role === "user" && <ChatUserPrompt text={m.message} />}
 
-          {m.role === "assistant" && <ChatAiAnswer text={m.content} />}
+          {m.role === "assistant" && <ChatAiAnswer text={m.message} />}
         </div>
       ))}
-      {error && <p className="text-red-500">{error.message}</p>}
 
       <InputArea text={text} setText={setText} handleOnClick={handleOnClick} />
     </>
